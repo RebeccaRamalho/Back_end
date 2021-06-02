47a1255fb709a821b48929c390a87a1f5655df71
eliezer-web <evj.97160@gmail.com>
2021-06-02 17:51:13 +0200

    wip

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  username: process.env.DB_USER,
+  user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
-db.connect((error) => error ? console.log(error):"connected not handofhope");
+db.connect((error) => error ? console.log(error):"connected to the handofhope db");
 
 module.exports = db.promise();
