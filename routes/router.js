b7c8ec371b912e4287735f1d3ea5d6c1da9018cd
RebeccaKanu <kanurebecca0@gmail.com>
2021-06-03 20:56:43 +0200

    wip

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-require('dotenv').config();
+require("dotenv").config();
 
-const controller = require("../controllers/controller");
+const controller = require("../controllers/userController");
 
 router.get("/articles", (req, res) => {
   const str = [
@@ -15,15 +15,10 @@ router.get("/articles", (req, res) => {
   res.end(JSON.stringify(str));
 });
 
+router
+  .post("/adminRegister", controller.signUp)
+  .post("/adminlogin", controller.login, controller.authentification);
 
- 
-router.post("/register", controller.login)
-      .post("/login", controller.login) 
-      .post("/Article", controller.addArticle) 
-      .delete("/Articles/:admin_id/:article_id",controller.delete_a_article)
-      .patch("/changeArticle/:article_id",controller.alter_a_article);
-// .post('')
-      // .get("/Article/availableArticle", controller.get_all_article);
-      
+// .get("/Article/availableArticle", controller.get_all_article);
 
 module.exports = router;
